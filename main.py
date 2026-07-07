e39df34f264933a2f90b4e8fd17df69409dcef01
Luana Reis <cealuanareiso2@gmail.com>
2026-07-07 14:42:26 -0300

    feat: adiciona orquestracao por comandos no main

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
+import sys
+
+from producer.coletor_news import coletar_e_enviar_noticias_para_kafka
 from producer.price_producer import coletar_e_enviar_precos_para_kafka
 
-coletar_e_enviar_precos_para_kafka()
+
+def main():
+    if len(sys.argv) < 2:
+        print("""
+Uso:
+
+python main.py noticias
+python main.py precos
+python main.py tudo
+""")
+        return
+
+    comando = sys.argv[1].lower()
+
+    if comando == "noticias":
+        coletar_e_enviar_noticias_para_kafka()
+
+    elif comando == "precos":
+        coletar_e_enviar_precos_para_kafka()
+
+    elif comando == "tudo":
+        coletar_e_enviar_noticias_para_kafka()
+        coletar_e_enviar_precos_para_kafka()
+
+    else:
+        print(f"Comando '{comando}' inválido.")
+
+
+if __name__ == "__main__":
+    main()
